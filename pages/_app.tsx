db3df503a22bc2b98d72a89f271cf3b359f8cc6c
Frederico Batista <rincofrederico@gmail.com>
2021-06-15 23:18:59 +0100

    chore: bootstrap project

---
+++ pages/_app.tsx
@@ -0,0 +1,7 @@
+import type { AppComponent } from 'next/dist/next-server/lib/router/router'
+
+const App: AppComponent = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />
+}
+
+export default App
