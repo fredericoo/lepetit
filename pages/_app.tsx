ad7c9116d0a3e20d444230a6bc18c61ac970def3
Frederico Batista <rincofrederico@gmail.com>
2021-06-16 21:46:19 +0100

    chore: setup chakra

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
-import type { AppComponent } from 'next/dist/next-server/lib/router/router'
+import type { AppComponent } from 'next/dist/next-server/lib/router/router';
+import { ChakraProvider } from '@chakra-ui/react';
 
 const App: AppComponent = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
-}
+  return (
+    <ChakraProvider>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+};
 
-export default App
+export default App;
