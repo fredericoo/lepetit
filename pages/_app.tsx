6d5610fbcf98a9a69f637c82bbbe2c020c113cea
Freddie <rincofrederico@gmail.com>
2021-12-12 18:31:15 +0000

    fix: removing footer and fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,17 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from 'app/lib/theme';
-import FontImports from 'app/components/FontImports';
+// import FontImports from 'app/components/FontImports';
 import Navbar from 'app/components/Navbar';
-import Footer from 'app/components/Footer';
+// import Footer from 'app/components/Footer';
 import { AppComponent } from 'next/dist/shared/lib/router/router';
 
 const App: AppComponent = ({ Component, pageProps }) => {
   return (
     <ChakraProvider theme={theme}>
-      <FontImports />
+      {/* <FontImports /> */}
       <Navbar />
       <Component {...pageProps} />
-      <Footer data={pageProps?.headerAndFooter?.data} />
+      {/* <Footer data={pageProps?.headerAndFooter?.data} /> */}
     </ChakraProvider>
   );
 };
