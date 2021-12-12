5a0cbf6ee5f9d5064626655ea25691e41b48b17a
Freddie <rincofrederico@gmail.com>
2021-12-12 18:39:40 +0000

    fix: replace font imports to @font-face

--- components/FontImports/FontImports.tsx
+++ components/FontImports/FontImports.tsx
@@ -1,7 +1,39 @@
 import { Global } from '@emotion/react';
 
 const FontImports: React.FC = () => {
-  return <Global styles={`@import url("https://use.typekit.net/vqe6zpb.css");`} />;
+  return (
+    <Global
+      styles={`@font-face {
+    font-family:"mr-eaves-modern";
+    src:url("https://use.typekit.net/af/5a0a8b/00000000000000007735b33f/30/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3") format("woff2"),url("https://use.typekit.net/af/5a0a8b/00000000000000007735b33f/30/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3") format("woff"),url("https://use.typekit.net/af/5a0a8b/00000000000000007735b33f/30/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3") format("opentype");
+    font-display:auto;font-style:normal;font-weight:400;
+    }
+    
+    @font-face {
+    font-family:"mr-eaves-modern";
+    src:url("https://use.typekit.net/af/555361/00000000000000007735b348/30/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff2"),url("https://use.typekit.net/af/555361/00000000000000007735b348/30/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("woff"),url("https://use.typekit.net/af/555361/00000000000000007735b348/30/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n7&v=3") format("opentype");
+    font-display:auto;font-style:normal;font-weight:700;
+    }
+    
+    @font-face {
+    font-family:"mrs-eaves-roman-all-small-ca";
+    src:url("https://use.typekit.net/af/347c50/00000000000000007735b2fa/30/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3") format("woff2"),url("https://use.typekit.net/af/347c50/00000000000000007735b2fa/30/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3") format("woff"),url("https://use.typekit.net/af/347c50/00000000000000007735b2fa/30/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3") format("opentype");
+    font-display:auto;font-style:normal;font-weight:400;
+    }
+    
+    @font-face {
+    font-family:"mrs-eaves-xl-serif";
+    src:url("https://use.typekit.net/af/b80a9d/00000000000000007735b2f3/30/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3") format("woff2"),url("https://use.typekit.net/af/b80a9d/00000000000000007735b2f3/30/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3") format("woff"),url("https://use.typekit.net/af/b80a9d/00000000000000007735b2f3/30/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3") format("opentype");
+    font-display:auto;font-style:normal;font-weight:400;
+    }
+    
+    @font-face {
+    font-family:"mrs-eaves-xl-serif-narrow";
+    src:url("https://use.typekit.net/af/d71eb9/00000000000000007735b2f8/30/l?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3") format("woff2"),url("https://use.typekit.net/af/d71eb9/00000000000000007735b2f8/30/d?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3") format("woff"),url("https://use.typekit.net/af/d71eb9/00000000000000007735b2f8/30/a?primer=7cdcb44be4a7db8877ffa5c0007b8dd865b3bbc383831fe2ea177f62257a9191&fvd=n4&v=3") format("opentype");
+    font-display:auto;font-style:normal;font-weight:400;
+    }`}
+    />
+  );
 };
 
 export default FontImports;
